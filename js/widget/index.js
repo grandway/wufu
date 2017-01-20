cee788a46aff401d2dc33a954648c658dcb4f011
yuhang zeng <yuhang zeng>
2017-01-20 20:58:21 +0800

    save

--- js/widget/index.js
+++ js/widget/index.js
@@ -195,8 +195,8 @@ function fuAdd() {
       getAllfu();
       // 测试
       window.wcShare.shareData = {
-        title: "集齐五福领88元现金",
-        desc: '新年福利(每个用户限领一次)还剩4570份',
+        title: "三部领10元新手现金红包",
+        desc: '新用户首次关注中欧云公众号领10元红包。',
         img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
         link: "http://mp.weixin.qq.com/s/JWXYvRX3NJ40Daq9TyM1vQ" //微交易
       }
