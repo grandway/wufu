52053b46ce34351f17f27646b142f8a3c6dd6e18
yuhang zeng <yuhang zeng>
2017-01-21 22:38:58 +0800

    save

--- js/widget/index.js
+++ js/widget/index.js
@@ -198,7 +198,7 @@ function fuAdd() {
         title: "三部领10元新手现金红包",
         desc: '新用户首次关注中欧云公众号领10元红包。',
         img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
-        link: "http://mp.weixin.qq.com/s/JWXYvRX3NJ40Daq9TyM1vQ" //微交易
+        link: "http://mp.weixin.qq.com/s/tOvVpCdeogmViOMl0EQ8Wg" //微交易
       }
       $('.go-ji').hide();
       if (shareTime >= 6 && step == 2) {
@@ -229,7 +229,7 @@ window.onload = function() {
         title: "三部领10元新手现金红包",
         desc: '新用户首次关注中欧云公众号领10元红包。',
         img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
-        link: "http://mp.weixin.qq.com/s/JWXYvRX3NJ40Daq9TyM1vQ" //微交易
+        link: "http://mp.weixin.qq.com/s/tOvVpCdeogmViOMl0EQ8Wg" //微交易
       }
         }
         shareTime++;
