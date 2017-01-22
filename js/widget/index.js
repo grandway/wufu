9384f15967b2da14758af093ec9e8ae980e0cd4f
yuhang zeng <yuhang zeng>
2017-01-22 12:58:34 +0800

    save

--- js/widget/index.js
+++ js/widget/index.js
@@ -95,7 +95,7 @@ window.wcShare = new WechatShare;
 var swiper;
 var step; //集齐五福之后走到了哪个步骤
  window.wcShare.shareData = {
-            title: "集⑤福送88",
+            title: "集⑤福领88人名币",
             desc: '新年福利(每个用户限领一次)',
             img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
             link: "http://xinianfu.duapp.com/" //微交易
