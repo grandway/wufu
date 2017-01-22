d92f0efc3c7fd28bdd52a63e4de0ddb918404fa0
yuhang zeng <yuhang zeng>
2017-01-22 18:22:03 +0800

    save

--- js/widget/index.js
+++ js/widget/index.js
@@ -4,10 +4,10 @@ Object.defineProperty(window, "WeixinJSBridge", {
   enumerable: !0,
   configurable: !0
 })
-var WechatShare = function() {
+var WechatShare = function () {
   function e() {
     var e = this;
-    this.onBridgeReady = function() {
+    this.onBridgeReady = function () {
         var t = window.WeixinJSBridge,
           a = {
             invoke: t.invoke,
@@ -33,7 +33,7 @@ var WechatShare = function() {
           e.restoreHandleMessageHookForWeixin()
         }
       },
-      this.handleMesageHook = function(t) {
+      this.handleMesageHook = function (t) {
         if (t) {
           var a;
           a = t.__json_message ? t.__json_message : t;
@@ -48,7 +48,7 @@ var WechatShare = function() {
               d = e.shareData.img_url,
               c = e.shareData.title;
             Object.defineProperty(o, "title", {
-                get: function() {
+                get: function () {
                   return delete o.scene,
                     o.desc = n,
                     o.link = r,
@@ -59,7 +59,7 @@ var WechatShare = function() {
                     }),
                     "title"
                 },
-                set: function() {},
+                set: function () {},
                 enumerable: !1,
                 configurable: !0
               }),
@@ -77,11 +77,11 @@ var WechatShare = function() {
           "WeixinJSBridgeReady", this.onBridgeReady),
         document.attachEvent("onWeixinJSBridgeReady", this.onBridgeReady)) : this.onBridgeReady()
   }
-  return e.prototype.setHandleMessageHookForWeixin = function() {
+  return e.prototype.setHandleMessageHookForWeixin = function () {
       this.oldHandleMesageHook = window.WeixinJSBridge._handleMessageFromWeixin,
         window.WeixinJSBridge._handleMessageFromWeixin = this.handleMesageHook
     },
-    e.prototype.restoreHandleMessageHookForWeixin = function() {
+    e.prototype.restoreHandleMessageHookForWeixin = function () {
       this.oldHandleMesageHook && (window.WeixinJSBridge._handleMessageFromWeixin = this.oldHandleMesageHook)
     },
     e
@@ -94,12 +94,22 @@ window.wcShare = new WechatShare;
 // 逻辑代码
 var swiper;
 var step; //集齐五福之后走到了哪个步骤
- window.wcShare.shareData = {
-            title: "集⑤福领💰",
-            desc: '新年福利(每个用户限领一次)',
-            img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
-            link: "http://jiwufu1.duapp.com/" //微交易
-          }
+window.wcShare.shareData = {
+  title: "集⑤福领💰",
+  desc: '新年福利(每个用户限领一次)',
+  img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
+  link: "http://jiwufu2.duapp.com/" //微交易
+}
+$.ajax({
+  url: "https://ssl5.ichuandian.com/Api/ThinUrl?value=" + encodeURIComponent(window.location.href.split('#')[0] + "?t=" + (new Date().getTime())),
+  type: 'get',
+  dataType: 'json',
+  success: function (data) {
+    if (data && data.ShareUrl) {
+      window.wcShare.shareData = data.ShareUrl;
+    }
+  }
+});
 window.localStorage.getItem('step') ? step = window.localStorage.getItem('step') : step = 1;
 swiper = new Swiper('.swiper-container1', {
   // pagination: '.swiper-pagination',
@@ -108,36 +118,36 @@ swiper = new Swiper('.swiper-container1', {
   paginationClickable: true,
   spaceBetween: 20,
   slidesOffsetBefor: 10,
-  onSlideChangeStart: function(swiper) {
+  onSlideChangeStart: function (swiper) {
     $('.swiper-wrapper-x').scrollLeft(swiper.activeIndex * 10);
     $('.in-swiper-item').eq(swiper.activeIndex).addClass('active').siblings().removeClass('active')
   }
 });
-$('.in-thumbs').on('click', '.in-swiper-item', function() {
+$('.in-thumbs').on('click', '.in-swiper-item', function () {
   var $this = $(this);
   swiper.slideTo($this.index())
 });
 //点击去集福
-$('.fu-box').click(function() {
+$('.fu-box').click(function () {
   if ($('.J_swiper-wrapper .swiper-slide').length == 5) {
     $('.fenxiang').show();
   }
 });
 //点击申请提现
-$(document).on('click', '.J_tixian-shen', function() {
-    $(".J_tixian").show();
-  })
-  //点击合成
-$('.mainBox').on('click', '.J_hecheng', function() {
+$(document).on('click', '.J_tixian-shen', function () {
+  $(".J_tixian").show();
+})
+//点击合成
+$('.mainBox').on('click', '.J_hecheng', function () {
   step = 2;
   window.localStorage.setItem('step', step);
   fuAdd()
 });
-$('.J_clearStorage').click(function() {
-    window.localStorage.clear();
-  })
-  // 集齐五福调用这个方法显示五福
-  // getAllfu();
+$('.J_clearStorage').click(function () {
+  window.localStorage.clear();
+})
+// 集齐五福调用这个方法显示五福
+// getAllfu();
 function getAllfu() {
   if ($('.J_swiper-wrapper .swiper-slide').length == 6) {
     return false;
@@ -180,12 +190,12 @@ function fuAdd() {
   if (!shareTime) {
     return false;
   } else {
-    $('.J_swiper-wrapper .swiper-slide').each(function(index) {
+    $('.J_swiper-wrapper .swiper-slide').each(function (index) {
       if (index < shareTime) {
         $(this).removeClass('in-no-has')
       }
     });
-    $('.in-swiper-item').each(function(index) {
+    $('.in-swiper-item').each(function (index) {
       if (index < shareTime) {
         $(this).addClass('in-has')
       }
@@ -216,8 +226,8 @@ if ($('.J_swiper-wrapper .swiper-slide').length == 5) {
 
 
 
-window.onload = function() {
-  window.wcShare && (window.wcShare.shareCallback = function(t) {
+window.onload = function () {
+  window.wcShare && (window.wcShare.shareCallback = function (t) {
     if (t.err_msg == "send_app_msg:ok" || t.err_msg == "send_app_msg:confirm" || t.err_msg == "share_timeline:confirm" || t.err_msg == "share_timeline:ok") {
       var shareTime = window.localStorage.getItem('shareTime')
       if (!shareTime) {
@@ -225,12 +235,12 @@ window.onload = function() {
       } else if (shareTime < 5) {
         if (shareTime == 4) {
           // 测试
-           window.wcShare.shareData = {
-        title: "闺蜜月工资才3200竟买了豪车，难道被包养???",
-        desc: '闺蜜小雅给我打电话说买车了，明天要带我兜风去，而且是买了一辆奔驰，打完电话，我还久久不能平静下来。',
-        img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
-        link: "http://mp.weixin.qq.com/s/VBtqy4-_d8fT6f8YnZ-bxA" //微交易
-      }
+          window.wcShare.shareData = {
+            title: "闺蜜月工资才3200竟买了豪车，难道被包养???",
+            desc: '闺蜜小雅给我打电话说买车了，明天要带我兜风去，而且是买了一辆奔驰，打完电话，我还久久不能平静下来。',
+            img_url: 'http://sa.gkdiandu.cn/static/img/red.png',
+            link: "http://mp.weixin.qq.com/s/VBtqy4-_d8fT6f8YnZ-bxA" //微交易
+          }
         }
         shareTime++;
         window.localStorage.setItem('shareTime', shareTime);
